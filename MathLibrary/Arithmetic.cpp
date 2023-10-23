33fc29d925abf065dc312c6d813583172d765a53
Александр <svinarev_aleksander_andreevich@mail.ru>
2023-10-23 20:02:01 +0700

    реализовал арифметические функции (+, -, *)

---
+++ MathLibrary/Arithmetic.cpp
@@ -0,0 +1,16 @@
+#include "Arithmetic.h"
+
+template<typename Type>
+Type sum(Type arg1, Type arg2) {
+	return arg1 + arg2;
+}
+
+template<typename Type>
+Type difference(Type arg1, Type arg2) {
+	return arg1 - arg2;
+}
+
+template<typename Type>
+Type multiplex(Type arg1, Type arg2) {
+	return arg1 * arg2;
+}
---
+++ MathLibrary/Arithmetic.h
@@ -0,0 +1,10 @@
+#pragma once
+
+template<typename Type>
+Type sum(Type arg1, Type arg2);
+
+template<typename Type>
+Type difference(Type arg1, Type arg2);
+
+template<typename Type>
+Type multiplex(Type arg1, Type arg2);
