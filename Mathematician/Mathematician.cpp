4d6ddf493072b8bf1b8872a3f22ead9c303f0a9c
Александр <svinarev_aleksander_andreevich@mail.ru>
2023-10-24 21:30:58 +0700

    добавил вывод буквы ё

--- Mathematician/Mathematician.cpp
+++ Mathematician/Mathematician.cpp
@@ -8,7 +8,7 @@ int main()
     int num1, num2;
     std::cin >> num1 >> num2;
     std::cout << "Sum: " << sum(num1, num2) << "\nDifference: " << difference(num1, num2) 
-        << "\nMultiplexing: " << multiplex(num1, num2) << "\nGrade: " << grade(num1, num2);
+        << "\nMultiplexing: " << multiplex(num1, num2) << "\nGrade: " << grade(num1, num2) << difference('d', 's');
 
     // From First_branch
     std::cout << "\nsin: " << sinus(num1) << " " << sinus(num2);
